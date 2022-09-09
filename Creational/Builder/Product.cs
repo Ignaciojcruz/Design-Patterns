93904ca0bdd43c10f60a76f8161bba6e63b1adf3
COMPASSGROUP\icruz <ignacio.cruz@cgcompass.com>
2022-09-09 11:51:56 -0400

    add Builder

---
+++ Creational/Builder/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            foreach (string part in _parts)
+            {
+                str += part + ", ";
+            }
+
+            str = str.Remove(str.Length - 2);
+
+            return "Product parts: " + str;
+        }
+    }
+}
