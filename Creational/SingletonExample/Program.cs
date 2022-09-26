7207213d99e85d48e65944c44304eabe13bfbb82
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-09-26 17:25:24 -0400

    add SingletonExample

---
+++ Creational/SingletonExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using SingletonExample.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TheBell b1 = TheBell.Instance;
+            TheBell b2 = TheBell.Instance;
+
+            if (b1 == b2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            b1.Ring();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/SingletonExample/Singleton/TheBell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample.Singleton
+{
+    internal sealed class TheBell
+    {
+        private static TheBell bellConnection;
+        private static object syncRoot = new object();
+
+        private TheBell() { }
+
+        public static TheBell Instance
+        {
+            get
+            {
+                lock(syncRoot)
+                {
+                    if(bellConnection == null)
+                    {
+                        bellConnection = new TheBell();
+                    }
+                }
+
+                return bellConnection;
+            }
+        }
+
+        public void Ring()
+        {
+            Console.WriteLine("Ding! Order Up!");
+        }
+    }
+}
