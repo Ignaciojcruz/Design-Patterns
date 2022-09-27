447443764e5728d709dc279cf2dc8870b90f2aeb
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-09-27 17:12:46 -0400

    add Adapter

---
+++ Structural/Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string getRequest()
+        {
+            return $"This is '{this._adaptee.GetEspecificRequest()}'";
+        }
+    }
+}
