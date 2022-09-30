4b6c3f754c553a46939059c68e62d2a1402bec85
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-09-30 17:32:42 -0400

    add AdapterExample

---
+++ Structural/AdapterExample/Client/Client.cs
@@ -0,0 +1,32 @@
+﻿using AdapterExample.Adapter;
+using AdapterExample.Target;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterExample.Client
+{
+    internal class Client
+    {
+        public static void MainClient()
+        {
+            //Non-adapted
+            Meat unknown = new Meat("Beef");
+            unknown.LoadData();
+
+            //Adapted
+            MeatDetails beef = new MeatDetails("Beef");
+            beef.LoadData();
+
+            MeatDetails turkey = new MeatDetails("Turkey");
+            turkey.LoadData();
+
+            MeatDetails chicken = new MeatDetails("Chicken");
+            chicken.LoadData();
+
+            Console.ReadKey();
+        }
+    }
+}
