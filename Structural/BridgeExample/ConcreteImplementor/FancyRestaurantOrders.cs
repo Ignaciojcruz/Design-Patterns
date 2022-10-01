a8dba654ea0dab55c22c55d74fc160519b313730
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-10-01 10:57:23 -0400

    add BridgeExample

---
+++ Structural/BridgeExample/ConcreteImplementor/FancyRestaurantOrders.cs
@@ -0,0 +1,18 @@
+﻿using BridgeExample.Implementor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeExample.ConcreteImplementor
+{
+    internal class FancyRestaurantOrders : IOrderingSystem
+    {
+        public void Place(string order)
+        {
+            Console.WriteLine("Placing order for "
+                              + order + " at the Fancy Restaurant.");
+        }
+    }
+}
