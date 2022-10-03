b1d6e31deca6f76ecb200b1e6f0bbad0243daf1e
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-10-03 18:01:30 -0400

    add Composite

---
+++ Structural/Composite/Component.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal abstract class Component
+    {
+        public Component() { }
+        public abstract string Operation();
+
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+
+    }
+}
