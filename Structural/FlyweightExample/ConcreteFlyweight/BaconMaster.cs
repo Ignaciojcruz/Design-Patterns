273dba4ed93d5417a909a3abe93e0adff0de245d
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-10-10 13:18:16 -0400

    Add FlyweightExample

---
+++ Structural/FlyweightExample/ConcreteFlyweight/BaconMaster.cs
@@ -0,0 +1,25 @@
+﻿using FlyweightExample.Flyweight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightExample.ConcreteFlyweight
+{
+    internal class BaconMaster : Slider
+    {
+        public BaconMaster()
+        {
+            Name = "Bacon Master";
+            Cheese = "American";
+            Toppings = "lots of bacon";
+            Price = 2.39m;
+        }
+
+        public override void Display(int orderTotal)
+        {
+            Console.WriteLine("Slider #" + orderTotal + ": " + Name + " - topped with " + Cheese + " cheese and " + Toppings + "! $" + Price.ToString());
+        }
+    }
+}
