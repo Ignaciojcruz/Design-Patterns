74c9824364bd802d555f8b80d63b2d7a38bbe26c
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-10-16 12:19:43 -0400

    add Command

---
+++ Behavioral/Command/SimpleCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    internal class SimpleCommand : ICommand
+    {
+        private string _payload;
+
+        public SimpleCommand(string payload)
+        {
+            _payload = payload;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
+        }
+    }
+}
