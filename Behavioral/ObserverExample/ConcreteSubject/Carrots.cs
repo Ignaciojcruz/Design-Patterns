18d15975ab8b10b40d916a3b338de6fc20dbfccc
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-11-11 17:56:43 -0400

    Add ObserverExample

---
+++ Behavioral/ObserverExample/ConcreteSubject/Carrots.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObserverExample.Subject;
+
+namespace ObserverExample.ConcreteSubject
+{
+    internal class Carrots : Veggies
+    {
+        public Carrots(double price) : base(price) { }
+    }
+}
---
+++ Behavioral/ObserverExample/Observer/IRestaurant.cs
@@ -0,0 +1,14 @@
+﻿using ObserverExample.Subject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample.Observer
+{
+    internal interface IRestaurant
+    {
+        void Update(Veggies veggies);
+    }
+}
