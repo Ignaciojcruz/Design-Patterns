edb21767ecb28c3a46cb07fb4dcbfdd182ee1e0e
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-11-13 12:27:35 -0400

    add State

---
+++ Behavioral/State/Context.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Context
+    {
+        private State _state = null;
+
+        public Context(State state)
+        {
+            TransitionTo(state);
+        }
+
+        public void TransitionTo(State state)
+        {
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+            _state = state;
+            _state.SetContext(this);
+        }
+
+        public void Request1()
+        {
+            _state.Handle1();
+        }
+
+        public void Request2()
+        {
+            _state.Handle2();
+        }
+    }
+}
