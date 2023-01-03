f8440d45aeb69e351f1fdd3dc0259fcbb9206db9
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-01-03 20:19:00 -0300

    Add StateExample

---
+++ Behavioral/StateExample/ConcreteState/Uncooked.cs
@@ -0,0 +1,46 @@
+﻿using StateExample.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateExample.ConcreteState
+{
+    internal class Uncooked : Doneness
+    {
+        public Uncooked(Doneness state)
+        {
+            currentTemp = state.CurrentTemp;
+            steak = state.Steak;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            lowerTemp = 0;
+            upperTemp = 130;
+            canEat = false;
+        }
+
+        public override void AddTemp(double amount)
+        {
+            currentTemp += amount;
+            DonenessCheck();
+        }
+
+        public override void RemoveTemp(double amount)
+        {
+            currentTemp -= amount;
+            DonenessCheck();
+        }
+
+        public override void DonenessCheck()
+        {
+            if (currentTemp > upperTemp)
+            {
+                steak.State = new Rare(this);
+            }
+        }
+    }
+}
---
+++ Behavioral/StateExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using StateExample.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let's cook a steak!
+            Steak account = new Steak("T-Bone");
+
+            // Apply temperature changes
+            account.AddTemp(120);
+            account.AddTemp(15);
+            account.AddTemp(15);
+            account.RemoveTemp(10); //Yes I know cooking doesn't work this way, bear with me.
+            account.RemoveTemp(15);
+            account.AddTemp(20);
+            account.AddTemp(20);
+            account.AddTemp(20);
+
+            Console.ReadKey();
+        }
+    }
+}
