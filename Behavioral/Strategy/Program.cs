cfc01ea0f4bff3502477746331a27794875f6ce2
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-01-16 20:39:24 -0300

    Add Strategy

---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+
+            Console.ReadKey();
+        }
+    }
+}
