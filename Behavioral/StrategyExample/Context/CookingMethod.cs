1962e4038a8d4d87a6644a198a6b18238a56769a
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-05-04 20:45:45 -0400

    add StrategyExample

---
+++ Behavioral/StrategyExample/Context/CookingMethod.cs
@@ -0,0 +1,31 @@
+﻿using StrategyExample.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyExample.Context
+{
+    internal class CookingMethod
+    {
+        private string food;
+        private CookStrategy cookStrategy;
+
+        public void SetCookStrategy(CookStrategy cookStrategy)
+        {
+            this.cookStrategy = cookStrategy;
+        }
+
+        public void setFood(string name)
+        {
+            this.food = name;
+        }
+
+        public void Cook()
+        {
+            cookStrategy.Cook(food);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Behavioral/StrategyExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using StrategyExample.Context;
+using StrategyExample.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CookingMethod method = new CookingMethod();
+
+            Console.WriteLine("What food would you like to cook?");
+            var food = Console.ReadLine();
+            method.setFood(food);
+
+            Console.WriteLine("What cooking strategy would you like to use (1-3)?");
+            int input = int.Parse(Console.ReadKey().KeyChar.ToString());
+
+            switch (input)
+            {
+                case 1:
+                    method.SetCookStrategy(new Grilling());
+                    method.Cook();
+                    break;
+
+                case 2:
+                    method.SetCookStrategy(new OvenBaking());
+                    method.Cook();
+                    break;
+                    
+                case 3:
+                    method.SetCookStrategy(new DeepFrying());
+                    method.Cook();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Selection!");
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
