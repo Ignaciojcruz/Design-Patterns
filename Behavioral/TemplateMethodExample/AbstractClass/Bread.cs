c330caf37a6fcc5a0abce648ac0064c162fe6696
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-05-06 23:29:43 -0400

    add TemplateMthod Example

---
+++ Behavioral/TemplateMethodExample/AbstractClass/Bread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodExample.AbstractClass
+{
+    internal abstract class Bread
+    {
+        public abstract void MixIngredients();
+
+        public abstract void Bake();
+
+        public virtual void Slice()
+        {
+            Console.WriteLine("Slicing the " + GetType().Name + " bread!");
+        }
+
+        //The template method
+        public void Make()
+        {
+            MixIngredients();
+            Bake();
+            Slice();
+        }
+    }
+}
---
+++ Behavioral/TemplateMethodExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateMethodExample.ConcreteClass;
+
+namespace TemplateMethodExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wheat = new WholeWheat();
+            wheat.Make();
+
+            Console.ReadKey();
+        }
+    }
+}
